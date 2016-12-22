187199dd4e9e9b8bbbc82e8083240a101e274a6e
Archimedes Trajano <developer@trajano.net>
2016-12-22 17:12:56 -0500

    Affixed header

--- app.js
+++ app.js
@@ -4,8 +4,22 @@ import './style.css'
 import 'materialize-css/dist/js/materialize'
 
 $(function () {
+
+    var navpos = $('#mainnav').offset();
+    var navheight = $('#mainnav').outerHeight();
+
+    $(window).bind('scroll', function () {
+        if ($(window).scrollTop() > navpos.top) {
+
+            $('#mainnav').addClass('navbar-fixed').addClass('affix-top')
+        }
+        else {
+            $('#mainnav').removeClass('navbar-fixed').removeClass('affix-top')
+        }
+    });
+
     $('.button-collapse').sideNav({
-         edge: 'right'
+        edge: 'right'
     });
     $('.parallax').parallax();
 })
