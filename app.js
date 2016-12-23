4f1afc159733e74394bbd4069ed6e06284af92af
Archimedes Trajano <developer@trajano.net>
2016-12-23 00:03:44 -0500

    Improved side nav

--- app.js
+++ app.js
@@ -16,4 +16,32 @@ $(function () {
         scrollOffset: 0
     })
     $('.parallax').parallax();
+
+    /* Obfuscate mailto and tel links */
+    $(".obfuscate").each(function () {
+        $(this).html($(this).html()
+            .replace("...", "@").replace(/\.\.\./g, ".")
+            .replace(/one/g, "1")
+            .replace(/two/g, "2")
+            .replace(/three/g, "3")
+            .replace(/four/g, "4")
+            .replace(/five/g, "5")
+            .replace(/six/g, "6")
+            .replace(/seven/g, "7")
+            .replace(/eight/g, "8")
+            .replace(/nine/g, "9")
+            .replace(/zero/g, "0"));
+        $(this).attr("href", $(this).attr("href")
+            .replace("...", "@").replace(/\.\.\./g, ".")
+            .replace(/one/g, "1")
+            .replace(/two/g, "2")
+            .replace(/three/g, "3")
+            .replace(/four/g, "4")
+            .replace(/five/g, "5")
+            .replace(/six/g, "6")
+            .replace(/seven/g, "7")
+            .replace(/eight/g, "8")
+            .replace(/nine/g, "9")
+            .replace(/zero/g, "0"));
+    });
 })
