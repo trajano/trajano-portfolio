c7eea5b8dfc41742c37576e654202eaee898aebb
Archimedes Trajano <developer@trajano.net>
2017-01-06 00:24:17 -0500

    Fixed resize handling and refactor obfuscation

--- src/app.js
+++ src/app.js
@@ -1,6 +1,26 @@
 import './app.scss'
 import 'materialize-css/dist/js/materialize'
 
+$(window).resize(function () {
+    $('#mainnav nav').pushpin('remove')
+    $('#mainnav nav').pushpin({
+        top: $('#mainnav').offset().top
+    })
+})
+function deobfuscate(e) {
+    return e.replace("...", "@")
+        .replace(/\.\.\./g, ".")
+        .replace(/One/g, "1")
+        .replace(/Two/g, "2")
+        .replace(/Three/g, "3")
+        .replace(/Four/g, "4")
+        .replace(/Five/g, "5")
+        .replace(/Six/g, "6")
+        .replace(/Seven/g, "7")
+        .replace(/Eight/g, "8")
+        .replace(/Nine/g, "9")
+        .replace(/Zero/g, "0")
+}
 $(function () {
     Waves.displayEffect({ duration: 200 })
     $('#mainnav nav').pushpin({
@@ -21,33 +41,11 @@ $(function () {
         window.location.href = $(this).data("href")
     })
 
-    /* Obfuscate mailto and tel links */
-    $(".obfuscate").each(function () {
-        $(this).html($(this).html()
-            .replace("...", "@").replace(/\.\.\./g, ".")
-            .replace(/One/g, "1")
-            .replace(/Two/g, "2")
-            .replace(/Three/g, "3")
-            .replace(/Four/g, "4")
-            .replace(/Five/g, "5")
-            .replace(/Six/g, "6")
-            .replace(/Seven/g, "7")
-            .replace(/Eight/g, "8")
-            .replace(/Nine/g, "9")
-            .replace(/Zero/g, "0"));
-        $(this).attr("href", $(this).attr("href")
-            .replace("...", "@").replace(/\.\.\./g, ".")
-            .replace(/One/g, "1")
-            .replace(/Two/g, "2")
-            .replace(/Three/g, "3")
-            .replace(/Four/g, "4")
-            .replace(/Five/g, "5")
-            .replace(/Six/g, "6")
-            .replace(/Seven/g, "7")
-            .replace(/Eight/g, "8")
-            .replace(/Nine/g, "9")
-            .replace(/Zero/g, "0"));
-    });
+    /* Deobfuscate mailto and tel links */
+    $("a.obfuscate").each(function () {
+        $(this).html(deobfuscate($(this).html()))
+        $(this).attr("href", deobfuscate($(this).attr("href")))
+    })
 
     // load stylesheet here
     const styleSheetLink = document.createElement('link')
@@ -65,7 +63,7 @@ $(function () {
     const mediaNetLink = document.createElement("script")
     mediaNetLink.type = "text/javascript"
     mediaNetLink.src = '//contextual.media.net/dmedianet.js?cid=8CU21S9US&https=1'
-    mediaNetLink.async = "async";
+    mediaNetLink.async = "async"
     const scriptAnchor = document.getElementsByTagName("script")[0]
     scriptAnchor.parentNode.insertBefore(mediaNetLink, scriptAnchor)
 
