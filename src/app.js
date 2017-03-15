1198952be9f831b9d916a3560cbe74581c992daa
Archimedes Trajano <arch@trajano.net>
2017-03-15 15:35:05 -0400

    Corrected globals

--- src/app.js
+++ src/app.js
@@ -75,7 +75,7 @@ $(function () {
         url: "//www.smartsuppchat.com/loader.js",
         cache: true,
         dataType: "script",
-        success: () => function() {
+        success: function() {
             var smartsupp
             smartsupp('on', 'status', status => {
                 if (status == 'online') {
