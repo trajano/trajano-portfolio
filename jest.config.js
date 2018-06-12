35e11685495ac8e190fc0a73af05d806f5c2aa22
Archimedes Trajano <arch@trajano.net>
2018-06-12 16:49:53 -0400

    chore(testing) added unit testing support via jest

---
+++ jest.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+    moduleFileExtensions: [
+      'js',
+      'json',
+      'vue'
+    ],
+    moduleNameMapper: {
+      '^@/(.*)$': '<rootDir>/src/$1'
+    },
+    transform: {
+      '^.+\\.js$': '<rootDir>/node_modules/babel-jest',
+      '.*\\.(vue)$': '<rootDir>/node_modules/vue-jest'
+    },
+    snapshotSerializers: ['<rootDir>/node_modules/jest-serializer-vue'],
+    //setupFiles: ['<rootDir>/test/unit/setup'],
+    coverageDirectory: '<rootDir>/test/unit/coverage',
+    collectCoverageFrom: [
+      'src/**/*.{js,vue}',
+      '!src/ld-json.js',
+      '!src/app.js',
+      '!**/node_modules/**'
+    ]
+  }
+  
---
+++ test/unit/specs/App.spec.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import App from '@/components/App'
+import '@/icons'
+
+describe('App.vue', () => {
+    it('should not crash', () => {
+      const Constructor = Vue.extend(App)
+      const vm = new Constructor({
+        propsData: {
+        }
+      }).$mount()
+    })
+  })
+  
