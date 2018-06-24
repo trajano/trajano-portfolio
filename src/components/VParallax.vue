ef6c2dcdd215b3f78ad3a7a1badc52723db7ac37
Archimedes Trajano <developer@trajano.net>
2018-06-23 21:48:22 -0400

    fix(parallax) corrected calculations

--- src/components/VParallax.vue
+++ src/components/VParallax.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="parallax-container">
+  <div class="vue-parallax-container">
     <slot />
-    <div class="parallax">
+    <div class="vue-parallax">
       <img :src="dataSrc" :alt="alt" @load="imageLoaded" :class="imageClass" :style="{transform}"/>
     </div>
   </div>
@@ -25,25 +25,38 @@ export default {
       transform: undefined
     }
   },
+  // computed: {
+  //   imgHeight() {
+  //     if (this.img) {
+  //       return this.img.offsetHeight
+  //     }
+  //   }
+  // },
   beforeDestroy() {
     this.unwatchVuex()
   },
   methods: {
     imageLoaded() {
       this.img.style.opacity = 1
-      this.imgHeight = this.img.offsetHeight
 
       this.updateParallax(this.$store.state.Window)
     },
     updateParallax({scrollTop, windowHeight}) {
+      if (!this.img) {
+        return
+      }
+      const containerTop = this.$el.offsetTop
+      const containerHeight = this.$el.offsetHeight
+      const containerBottom = containerTop + containerHeight
+
+      const imgHeight = this.img.offsetHeight
       const windowBottom = scrollTop + windowHeight
-      const parallaxDist = this.imgHeight - this.containerHeight
+      const parallaxDist = imgHeight - containerHeight
 
       const percentScrolled =
-        (windowBottom - this.top) / (this.containerHeight + windowHeight)
+        (windowBottom - containerTop) / (containerHeight + windowHeight)
       const parallax = parallaxDist * percentScrolled
-
-      this.transform = `translate3D(-50%, ${parallax}px, 0)`
+      this.transform = `translate(-50%, ${parallax}px)`
     }
   },
   mounted() {
@@ -51,8 +64,6 @@ export default {
       return
     }
     this.dataSrc = this.src
-    this.containerHeight = this.$el.offsetHeight
-    this.top = this.$el.getBoundingClientRect().top
     this.img = this.$el.children
       .item(this.$el.children.length - 1)
       .children.item(0)
@@ -69,3 +80,30 @@ export default {
   }
 }
 </script>
+<style lang="scss" scoped>
+.vue-parallax-container {
+  position: relative;
+  overflow: hidden;
+  min-height: 380px;
+  line-height: 0;
+  height: auto;
+  color: rgba(255,255,255,.9);
+
+  .vue-parallax {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    z-index: -1;
+  
+    img {
+      position: absolute;
+      left: 50%;
+      bottom: 0;
+      min-width: 100%;
+      min-height: 100%;
+    }
+  }
+}
+</style>
