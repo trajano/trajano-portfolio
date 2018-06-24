9c03773d41ad6bd689b762149de4a0ddf7d8ae2c
Archimedes Trajano <developer@trajano.net>
2018-06-24 18:38:48 -0400

    fix(app) remove jquery resize handler

--- src/app.js
+++ src/app.js
@@ -5,7 +5,6 @@ import 'materialize-css'
 import './icons'
 import './directives'
 import App from './components/App'
-import $ from 'jquery'
 import store from './store'
 
 /* eslint-disable no-new */
@@ -17,11 +16,4 @@ new Vue({
     // You'll need this for renderAfterDocumentEvent.
     document.dispatchEvent(new Event('render-event'))
   }
-})
-
-$(window).resize(() => {
-  $('#mainnav-nav').pushpin('remove')
-  $('#mainnav-nav').pushpin({
-    top: $('#mainnav').offset().top
-  })
 })
